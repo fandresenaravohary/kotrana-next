fbc284aef4c731e46e4db3fcc63964f20bb167a3
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-08-22 10:51:06 +0300

    chore: update project

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import {ReactNode} from 'react';
+import { DashboardLayout } from './_components/dashboard-layout';
+
+type LayoutProps = {
+    children: ReactNode;
+}
+
+const Layout = ({children} : LayoutProps) => {
+    return <DashboardLayout>
+    
+    {children}
+
+    </DashboardLayout>;
+};
+
+export default Layout;
---
+++ src/lib/db.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "$/generated/prisma";
+import { Prisma } from "@prisma/client/extension";
+
+const prismaClientSingleton = () => {
+    return new PrismaClient();
+}
+
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global;
+
+const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default db;
+
+if (process.env.NODE_ENV !== "production") {
+    globalThis.prismaGlobal = db;
+}
