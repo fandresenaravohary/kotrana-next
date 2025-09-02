95065f3f78d95b6e03eeffe7650d195e04f963db
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-09-02 11:42:10 +0300

    chore: add skeleton for category

--- src/app/(dashboard)/admin/food-management/categories/_components/category-cards.tsx
+++ src/app/(dashboard)/admin/food-management/categories/_components/category-cards.tsx
@@ -6,47 +6,55 @@ import { useCategories } from "../_services/use-category-queries";
 import { Edit, Trash } from "lucide-react";
 import { alert } from "@/lib/use-global-store";
 import { useCategoriesStore } from "../_libs/use-category-store";
+import CategoryCardsSkeleton from "./category-cards-skeleton";
 
 const CategoryCards = () => {
-  const { updateSelectedCategoryId, updateCategoryDialogOpen } = useCategoriesStore();
+  const { updateSelectedCategoryId, updateCategoryDialogOpen } =
+    useCategoriesStore();
   const categoriesQuery = useCategories();
   const deleCategoryMutation = useDeleteCategory();
 
   return (
     <div className="grid grid-cols-4 gap-2">
-      {categoriesQuery.data?.map((item) => (
-        <div
-          key={item.id}
-          className="bg-accent flex flex-col justify-between gap-3 rounded-lg p-6 shadow-md"
-        >
-          <p className="truncate">{item.name}</p>
-          <div className="flex gap-1">
-            <Button
-              className="size-6"
-              variant={"ghost"}
-              size={"icon"}
-              onClick={() => {
-                updateSelectedCategoryId(item.id);
-                updateCategoryDialogOpen(true);
-              }}
+      {categoriesQuery.isLoading ? (
+        <CategoryCardsSkeleton />
+      ) : (
+        <>
+          {categoriesQuery.data?.map((item) => (
+            <div
+              key={item.id}
+              className="bg-accent flex flex-col justify-between gap-3 rounded-lg p-6 shadow-md"
             >
-              <Edit />
-            </Button>
-            <Button
-              className="size-6"
-              variant={"ghost"}
-              size={"icon"}
-              onClick={() => {
-                alert({
-                  onConfirm: () => deleCategoryMutation.mutate(item.id),
-                });
-              }}
-            >
-              <Trash />
-            </Button>
-          </div>
-        </div>
-      ))}
+              <p className="truncate">{item.name}</p>
+              <div className="flex gap-1">
+                <Button
+                  className="size-6"
+                  variant={"ghost"}
+                  size={"icon"}
+                  onClick={() => {
+                    updateSelectedCategoryId(item.id);
+                    updateCategoryDialogOpen(true);
+                  }}
+                >
+                  <Edit />
+                </Button>
+                <Button
+                  className="size-6"
+                  variant={"ghost"}
+                  size={"icon"}
+                  onClick={() => {
+                    alert({
+                      onConfirm: () => deleCategoryMutation.mutate(item.id),
+                    });
+                  }}
+                >
+                  <Trash />
+                </Button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
